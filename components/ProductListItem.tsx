0f9a9526d477b1eb4ed1b67439c671e3089354f4
Safe Gergis <safegerg@gmail.com>
2024-03-14 14:26:44 -0700

    type safe component

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 
 import Colors from "@/constants/Colors";
+import { Product } from "@/types";
+type ProductListItemProps = {
+  product: Product;
+};
 
-const ProductListItem = ({ product }) => {
+const defaultImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image}></Image>
+      <Image
+        source={{ uri: product.image ?? defaultImage }}
+        style={styles.image}
+      ></Image>
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
