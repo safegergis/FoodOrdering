69acdf9359626739a57fe18d7d9fc896d2d0cc74
Safe Gergis <safegerg@gmail.com>
2024-03-14 17:49:59 -0700

    dynamic route for product details

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,7 @@
-import { StyleSheet, View } from "react-native";
+import { Redirect } from "expo-router";
 
-import ProductListItem from "@/components/ProductListItem";
-import Colors from "@/constants/Colors";
-import products from "@/assets/data/products";
+const TabIndex = () => {
+  return <Redirect href={"/menu/"} />;
+};
 
-export default function MenuScreen() {
-  return (
-    <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  price: {
-    color: Colors.light.tint,
-  },
-  image: { width: "100%", aspectRatio: 1 },
-});
+export default TabIndex;
--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { Link } from "expo-router";
 import Colors from "@/constants/Colors";
 import { Product } from "@/types";
 type ProductListItemProps = {
@@ -8,16 +8,20 @@ type ProductListItemProps = {
 
 const defaultImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image ?? defaultImage }}
-        style={styles.image}
-      ></Image>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image ?? defaultImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -27,6 +31,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 18,
